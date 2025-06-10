f7248d57c0ebb6f2feb57d6d397066e6e90e3e8b
xunweiwong <wong1155@flinders.edu.au>
2025-06-10 18:31:43 +0930

    Added first 6 test classes to test white box testing

---
+++ src/test/java/racesimulation/AerodynamicKitTest.java
@@ -0,0 +1,32 @@
+package racesimulation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AerodynamicKitTest {
+
+    @Test
+    void getKitName() {
+    }
+
+    @Test
+    void getDragCoefficient() {
+    }
+
+    @Test
+    void getDownforceValue() {
+    }
+
+    @Test
+    void getImpactOnTopSpeed() {
+    }
+
+    @Test
+    void getImpactOnCornering() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
